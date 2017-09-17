d6235993953698ced518aec1a09e07780556ed36
hli2020 <hongyangli2020@gmail.com>
2017-09-17 13:53:12 +0800

    tiny from s31

---
+++ scripts/renew_512_set2_test_only.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+start=`date +%s`
+
+CUDA_VISIBLE_DEVICES=1 python eval.py --experiment_name=renew_512_set2 \
+--trained_model=ssd512_0712_iter_85000.pth \
+--ssd_dim=512 --conf_thresh=0.005 --top_k=300 --nms_thresh=0.45 \
+--prior_config=v2_512
+
+CUDA_VISIBLE_DEVICES=1 python eval.py --experiment_name=renew_512_set2 \
+--trained_model=ssd512_0712_iter_65000.pth \
+--ssd_dim=512 --conf_thresh=0.005 --top_k=300 --nms_thresh=0.45 \
+--prior_config=v2_512
+
+
+end=`date +%s`
+runtime=$((end-start))
+
+echo "Completed in" $runtime "seconds"
