bc847ba16e91b9e3c604b5a6ea2537502664323b
hli2020 <hongyangli2020@gmail.com>
2017-11-12 00:13:43 +0800

    redegisn the structure of capsule network

--- layers/modules/capsule.py
+++ layers/modules/capsule.py
@@ -30,7 +30,7 @@ class CapsNet(nn.Module):
         else:
             self.cap_model = 'v0'
         self.cap_N = opts.cap_N
-        self.structure = opts.model_cifar   # capsule
+        self.structure = opts.model_cifar   # capsule or resnet
         self.skip_pre_squash = opts.skip_pre_squash
         self.skip_pre_transfer = opts.skip_pre_transfer
 
@@ -44,77 +44,19 @@ class CapsNet(nn.Module):
             self.avgpool = nn.AvgPool2d(6)
             self.fc = nn.Linear(256, num_classes)
         if self.cap_model == 'v0':
-            # capsule module
+            # original capsule idea in the paper
             self.cap_layer = CapLayer(opts, num_in_caps=32*6*6, num_out_caps=num_classes,
-                                      in_dim=8, out_dim=16,
-                                      num_shared=32)
-        # different structures below
-        if self.cap_model == 'v1' or self.cap_model == 'v2' or self.cap_model == 'v5':
-            self.cap_dim_1 = 16
-            # transfer convolution to capsule
-            self.transfer1_1 = nn.Sequential(*[
-                nn.Conv2d(16, 16, kernel_size=3, padding=1),
-                # nn.BatchNorm2d(int(16*self.cap_dim)),
-                # nn.ReLU(True)
-            ])
-            self.cap1 = CapLayer2(16, self.cap_dim_1, 32, 16,
-                                  route_num=opts.route_num, b_init=opts.b_init, w_version=opts.w_version)
-            # transfer capsule to convolution
-            self.transfer1_2 = nn.Sequential(*[
-                nn.ConvTranspose2d(self.cap_dim_1, 16, kernel_size=3,
-                                   padding=1, stride=2, output_padding=1),
-                nn.BatchNorm2d(16),
+                                      in_dim=8, out_dim=16, num_shared=32)
+        else:
+            # different structures below
+            self.buffer = nn.Sequential(*[
+                nn.Conv2d(64, 64, kernel_size=1),
                 nn.ReLU(True)
             ])
-
-            self.cap_dim_2 = 32
-            self.transfer2_1 = nn.Sequential(*[
-                nn.Conv2d(32, 32, kernel_size=3, padding=1),
-            ])
-            self.cap2 = CapLayer2(32, self.cap_dim_2, 16, 8,
-                                  route_num=opts.route_num, b_init=opts.b_init, w_version=opts.w_version)
-            self.transfer2_2 = nn.Sequential(*[
-                nn.ConvTranspose2d(self.cap_dim_2, 32, kernel_size=3,
-                                   padding=1, stride=2, output_padding=1),
-                nn.BatchNorm2d(32),
-                nn.ReLU(True)
-            ])
-
-            self.cap_dim_3 = 64
-            self.transfer3_1 = nn.Sequential(*[
-                nn.Conv2d(64, 64, kernel_size=3, padding=1),
-            ])
-            self.cap3 = CapLayer2(64, self.cap_dim_3, 8, 4,
-                                  route_num=opts.route_num, b_init=opts.b_init, w_version=opts.w_version)
-            self.transfer3_2 = nn.Sequential(*[
-                nn.ConvTranspose2d(self.cap_dim_3, 64, kernel_size=3,
-                                   padding=1, stride=2, output_padding=1),
-                nn.BatchNorm2d(64),
-                nn.ReLU(True)
-            ])
-        if self.cap_model == 'v2' or self.cap_model == 'v5':
-            self.cap_dim_4 = 64
-            self.cap4 = CapLayer2(64, self.cap_dim_4, 4, 10, as_final_output=True,
-                                  route_num=opts.route_num, b_init=opts.b_init, w_version=opts.w_version)
-        # THERE IS NO VERSION 3
-        if self.cap_model == 'v4':
-            self.cap_v4_dim = 64
-            self.transfer_v4 = nn.Sequential(*[
-                nn.Conv2d(64, 64, kernel_size=3, padding=1),
-            ])
-            self.cap_v4_1 = CapLayer2(64, self.cap_v4_dim, 8, 8,
-                                      route_num=opts.route_num, b_init=opts.b_init, w_version=opts.w_version)
-            self.cap_v4_2 = CapLayer2(self.cap_v4_dim, self.cap_v4_dim, 8, 10, as_final_output=True,
-                                      route_num=opts.route_num, b_init=opts.b_init, w_version=opts.w_version)
-        if self.cap_model == 'v5':
-            self.cap_v5_dim = 64
-            self.long_cap = CapLayer2(self.cap_v5_dim, self.cap_v5_dim, 4, 4,
-                                      route_num=opts.route_num, b_init=opts.b_init, w_version=opts.w_version)
-            self.bucket = nn.Sequential(*[
-                nn.Conv2d(self.cap_v5_dim, self.cap_v5_dim, kernel_size=1),
-                nn.BatchNorm2d(self.cap_v5_dim),
-                nn.ReLU(True)
-            ])
+            # then do squash in the forward pass
+            # the new convolution capsule idea
+            self.basic_cap = CapLayer2(64, 64, 8, 8, route_num=opts.route_num)
+            self.cls_cap = CapLayer2(64, 64, 8, 10, as_final_output=True, route_num=opts.route_num)
 
         # init the network
         for m in self.modules():
@@ -140,34 +82,8 @@ class CapsNet(nn.Module):
         x = self.layer2(x)                  # 32 x 16 x 16
         x = self.layer3(x)                  # bs x 64(for depth=20) x 8 x 8
 
-        if self.cap_model == 'v1' or \
-                self.cap_model == 'v2' or \
-                self.cap_model == 'v5':
-
-            if not self.skip_pre_transfer:
-                x = self.transfer3_1(x)
-            if not self.skip_pre_squash:
-                x = self._do_squash(x)
-            x = self.cap3(x)                # bs, 64, 4, 4
-            if self.cap_model == 'v1':
-                x = self.transfer3_2(x)     # bs, 64, 8, 8
-
-        if self.cap_model == 'v2':
-            x = self.cap4(x)
-        elif self.cap_model == 'v4':
-            if not self.skip_pre_transfer:
-                x = self.transfer_v4(x)
-            if not self.skip_pre_squash:
-                x = self._do_squash(x)
-            x = self.cap_v4_1(x)
-            x = self.cap_v4_2(x)
-        elif self.cap_model == 'v5':
-            for i in range(self.cap_N):
-                x = self.long_cap(x)
-                x = self.bucket(x)
-            x = self.cap4(x)   # use the head of v2
-        else:
-            # v1, v0, and resnet.
+        # THE FOLLOWING ARE PARALLEL TO EACH OTHER
+        if self.structure == 'resnet' or self.cap_model == 'v0':
             x = self.tranfer_conv(x)
             x = self.tranfer_bn(x)
             x = self.tranfer_relu(x)        # bs x 64 x 6 x 6
@@ -180,9 +96,18 @@ class CapsNet(nn.Module):
                 x = self.avgpool(x)
                 x = x.view(x.size(0), -1)
                 x = self.fc(x)
+
+        if self.cap_model == 'v1':
+            x = self.buffer(x)
+            x = self._do_squash(x)
+            x = self.basic_cap(x)
+            x = self.cls_cap(x)
+        else:
+            raise NameError('Unknown structure or capsule model type.')
         return x, stats
 
     def _do_squash(self, x):
+        # do squash along the channel dimension
         spatial_size = x.size(2)
         input_channel = x.size(1)
         x = x.resize(x.size(0), x.size(1), int(spatial_size**2)).permute(0, 2, 1)
