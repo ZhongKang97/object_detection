435e59e6ba52b1cfea74a4f245f6e3c22fbe504e
hli2020 <hongyangli2020@gmail.com>
2017-11-11 15:39:22 +0800

    deploy spread loss

--- scripts/Nov_11/cifar_base_104_no_relu_spread.sh
+++ scripts/Nov_11/cifar_base_104_no_relu_spread.sh
@@ -3,13 +3,14 @@
 start=`date +%s`
 
 # train and test
-CUDA_VISIBLE_DEVICES=3 python holly_cifar.py \
+CUDA_VISIBLE_DEVICES=0 python holly_cifar.py \
 --experiment_name=cifar_base_104_no_relu_spead \
---dataset=cifar \
+--dataset=cifar10 \
 --model_cifar=capsule \
 --epochs=300 \
+--optim=sgd \
 --schedule_cifar 150 225 \
---lr=0.001 \
+--lr=0.0001 \
 --use_spread_loss \
 --deploy
 
