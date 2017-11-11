16626020d5d88d40a3d6d89a3960179e56d14a5e
hli2020 <hongyangli2020@gmail.com>
2017-11-11 16:18:58 +0800

    update

--- scripts/Nov_11/cifar_base_104_no_relu_spread.sh
+++ scripts/Nov_11/cifar_base_104_no_relu_spread.sh
@@ -3,12 +3,12 @@
 start=`date +%s`
 
 # train and test
-CUDA_VISIBLE_DEVICES=0 python holly_cifar.py \
---experiment_name=cifar_base_104_no_relu_spead \
+CUDA_VISIBLE_DEVICES=2 python holly_cifar.py \
+--experiment_name=cifar_base_104_no_relu_spead_rmsprop \
 --dataset=cifar10 \
 --model_cifar=capsule \
 --epochs=300 \
---optim=sgd \
+--optim=rmsprop \
 --schedule_cifar 150 225 \
 --lr=0.0001 \
 --use_spread_loss \
