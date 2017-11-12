e88d67206036d8644636f62364b654b578e48859
hli2020 <hongyangli2020@gmail.com>
2017-11-12 15:04:51 +0800

    udpate

--- scripts/Nov_11/cifar_base_104_no_relu_multi_crop.sh
+++ scripts/Nov_11/cifar_base_104_no_relu_multi_crop.sh
@@ -4,16 +4,16 @@ start=`date +%s`
 
 # train and test
 # '--test_batch=32' has to be small on cifar100
-CUDA_VISIBLE_DEVICES=5 python holly_cifar.py \
---experiment_name=cifar_base_104_no_relu_multi_crop_fmnist \
---dataset=fmnist \
+CUDA_VISIBLE_DEVICES=6 python holly_cifar.py \
+--experiment_name=cifar_base_104_no_relu_multi_crop_cifar100 \
+--dataset=cifar100 \
 --model_cifar=capsule \
 --epochs=300 \
 --schedule_cifar 150 225 \
 --lr=0.01 \
 --route_num=4 \
 --train_batch=128 \
---test_batch=128 \
+--test_batch=32 \
 --w_version=v2 \
 --multi_crop_test \
 --deploy
