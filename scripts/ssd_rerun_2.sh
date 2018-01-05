e82bb1bf646d45239edface49eea2f32d17e461d
hli2020 <hongyangli2020@gmail.com>
2018-01-05 16:41:12 +0800

    udpate

--- scripts/ssd_rerun_2.sh
+++ scripts/ssd_rerun_2.sh
@@ -4,7 +4,7 @@ start=`date +%s`
 
 NAME=ssd_rerun_2
 # train
-CUDA_VISIBLE_DEVICES=0,1 python train.py \
+CUDA_VISIBLE_DEVICES=0,1,2,3 python train.py \
 --experiment_name=$NAME --debug_mode=False \
 --dataset=coco \
 --batch_size=32 --lr=0.0001 \
